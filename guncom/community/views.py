7e5881956f539d3bb1e83a60eae64707e9571432
admin <rnseodydrPwjd1@gmail.com>
2021-07-30 04:33:41 +0000

    stop using httpResponse func, render function setting add app
    render(requests, 'community/index.html')
    community/templates/community/index.html

--- guncom/community/views.py
+++ guncom/community/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>hey</h2>")
+    return render(request, 'community/index.html')
